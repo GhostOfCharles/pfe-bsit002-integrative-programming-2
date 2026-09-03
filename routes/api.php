48569cdd9d4a42c604cda796485dc1e4210d19f1
GhostOfCharles <charlesktn.2006@gmail.com>
2026-09-03 22:30:56 +0800

    feat: implement Employee model, migration, controller CRUD, and API routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\EmployeeController;
 
 Route::get('/students', function () {
     return response()->json([
@@ -16,3 +17,10 @@ Route::get('/students', function () {
         ]
     ]);
 });
+
+
+Route::get('/employees', [EmployeeController::class, 'index']);
+Route::post('/employees', [EmployeeController::class, 'store']);
+Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
