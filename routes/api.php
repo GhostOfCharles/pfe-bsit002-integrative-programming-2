a89e366425cbade05be7dd5f43f29adf75488226
GhostOfCharles <charlesktn.2006@gmail.com>
2026-09-05 16:59:09 +0800

    feat: implement Sanctum API authentication and middleware

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EmployeeController;
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::get('/students', function () {
     return response()->json([
@@ -18,9 +24,14 @@ Route::get('/students', function () {
     ]);
 });
 
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::get('/employees', [EmployeeController::class, 'index']);
-Route::post('/employees', [EmployeeController::class, 'store']);
-Route::get('/employees/{id}', [EmployeeController::class, 'show']);
-Route::put('/employees/{id}', [EmployeeController::class, 'update']);
-Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/employees', [EmployeeController::class, 'index']);
+    Route::post('/employees', [EmployeeController::class, 'store']);
+    Route::get('/employees/{id}', [EmployeeController::class, 'show']);
+    Route::put('/employees/{id}', [EmployeeController::class, 'update']);
+    Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']);
+
+});
